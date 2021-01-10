aa0ef7bc0921ec0ce0c13151e3577f2d88382b02
Mush <64996140+MushhRa@users.noreply.github.com>
2021-01-10 16:45:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import discord
 import os
 import random
 from discord.ext import commands
+
 client = discord.Client()
 client = commands.Bot(command_prefix="$") 
 
 @client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$fuck'):
+        await message.channel.send('you')
 
 @client.command()
 async def coinflip(ctx):
